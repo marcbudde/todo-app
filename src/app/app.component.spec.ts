35f36e1b460447859a8cb2bcae5942cdbe4f1e8b
Budde, Marc André <marcandre.budde@it.nrw.de>
2023-10-20 00:30:46 +0200

    added title again

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,6 +28,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('awesome-app app is running!');
+    expect(compiled.querySelector('h2.app_title')?.textContent).toContain('awesome-app');
   });
 });
